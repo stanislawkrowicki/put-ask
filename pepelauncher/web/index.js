a8e6cd08622d676a834c91ea6e69eb758e230cbb
Stanisław Krowicki <krowicki00@gmail.com>
2025-05-06 21:01:26 +0200

    Add text inside game image on index; hover effect

--- pepelauncher/web/index.js
+++ pepelauncher/web/index.js
@@ -7,17 +7,26 @@ const loadGames = async () => {
     const games = await eel.get_games()()
 
     games.forEach((game) => {
-        const a = document.createElement('a')
-        a.href = `game.html?game=${game}`
-        a.className = 'game'
+        const a = document.createElement('a');
+        a.href = `game.html?game=${game}`;
+        a.className = 'game';
+        
+        const img = document.createElement('img');
+        img.src = `img/games/${game}.png`;
+        img.alt = game;
+        img.className = 'game-img';
 
-        const img = document.createElement('img')
-        img.src = `img/games/${game}.png`
-        img.alt = game
-        img.className = 'game-img'
-
-        a.appendChild(img)
-        document.querySelector('#games').appendChild(a)
+        const imgContainer = document.createElement('div');
+        imgContainer.className = 'game-img-container';
+        
+        const overlayText = document.createElement('span');
+        overlayText.className = 'game-overlay-text';
+        overlayText.textContent = game;
+        
+        imgContainer.appendChild(img);
+        imgContainer.appendChild(overlayText);
+        a.appendChild(imgContainer);
+        document.querySelector('#games').appendChild(a);
     })
 }
 
