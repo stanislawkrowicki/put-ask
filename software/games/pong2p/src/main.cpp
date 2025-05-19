e177d1d343ede7bc366d51619adaa6d4881278a3
Stanisław Krowicki <krowicki00@gmail.com>
2025-05-19 19:20:45 +0200

    Fix pinouts in games

--- software/games/pong2p/src/main.cpp
+++ software/games/pong2p/src/main.cpp
@@ -5,16 +5,15 @@
 #include <OTAServer.h>
 #include "wifi_secrets.h"
 
+#define BTN_LEFT_1 27
+#define BTN_RIGHT_1 35
+#define BTN_LEFT_2 32
+#define BTN_RIGHT_2 14
 
-#define BTN_LEFT_1 16
-#define BTN_RIGHT_1 17
-#define BTN_LEFT_2 18
-#define BTN_RIGHT_2 19
-
-#define SCREEN_WIDTH  240
+#define SCREEN_WIDTH 240
 #define SCREEN_HEIGHT 320
 
-#define PADDLE_WIDTH  60
+#define PADDLE_WIDTH 60
 #define PADDLE_HEIGHT 6
 #define BALL_SIZE 6
 
@@ -24,8 +23,7 @@
 OTAServer otaServer;
 TFT_eSPI tft = TFT_eSPI();
 
-
-//pong globals
+// pong globals
 
 int p1X = (SCREEN_WIDTH - PADDLE_WIDTH) / 2;
 int p2X = (SCREEN_WIDTH - PADDLE_WIDTH) / 2;
@@ -60,7 +58,7 @@ void setup()
   pinMode(BTN_RIGHT_2, INPUT_PULLUP);
 
   tft.init();
-  tft.setRotation(0); //vertical
+  tft.setRotation(0); // vertical
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE);
   tft.setTextSize(2);
@@ -70,48 +68,60 @@ void setup()
   tft.fillScreen(TFT_BLACK);
 }
 
-void update_paddles() {
-  if (digitalRead(BTN_LEFT_1) == LOW && p1X > 0) p1X -= 5;
-  if (digitalRead(BTN_RIGHT_1) == LOW && p1X + PADDLE_WIDTH < SCREEN_WIDTH) p1X += 5;
-  if (digitalRead(BTN_LEFT_2) == LOW && p2X > 0) p2X -= 5;
-  if (digitalRead(BTN_RIGHT_2) == LOW && p2X + PADDLE_WIDTH < SCREEN_WIDTH) p2X += 5;
+void update_paddles()
+{
+  if (digitalRead(BTN_LEFT_1) == LOW && p1X > 0)
+    p1X -= 5;
+  if (digitalRead(BTN_RIGHT_1) == LOW && p1X + PADDLE_WIDTH < SCREEN_WIDTH)
+    p1X += 5;
+  if (digitalRead(BTN_LEFT_2) == LOW && p2X > 0)
+    p2X -= 5;
+  if (digitalRead(BTN_RIGHT_2) == LOW && p2X + PADDLE_WIDTH < SCREEN_WIDTH)
+    p2X += 5;
 }
-void reset_ball() {
+void reset_ball()
+{
   ballX = SCREEN_WIDTH / 2;
   ballY = SCREEN_HEIGHT / 2;
   ballDx = random(0, 2) == 0 ? 2.2 : -2.2;
   ballDy = random(0, 2) == 0 ? 2.6 : -2.6;
 }
 
-void move_ball() {
+void move_ball()
+{
   ballX += ballDx;
   ballY += ballDy;
 
   // Bounce off side walls
-  if (ballX <= 0 || ballX >= SCREEN_WIDTH) {
+  if (ballX <= 0 || ballX >= SCREEN_WIDTH)
+  {
     ballDx = -ballDx;
   }
 
   // Player 1 paddle collision (moving upward)
-  if (ballDy < 0 &&  // Only bounce if moving upward
+  if (ballDy < 0 && // Only bounce if moving upward
       ballY - BALL_SIZE <= P1_Y + PADDLE_HEIGHT &&
       ballY - BALL_SIZE >= P1_Y &&
-      ballX >= p1X && ballX <= p1X + PADDLE_WIDTH) {
-    ballDy = abs(ballDy);  // Bounce down
+      ballX >= p1X && ballX <= p1X + PADDLE_WIDTH)
+  {
+    ballDy = abs(ballDy); // Bounce down
   }
 
   // Player 2 paddle collision (moving downward)
-  if (ballDy > 0 &&  // Only bounce if moving downward
+  if (ballDy > 0 && // Only bounce if moving downward
       ballY + BALL_SIZE >= P2_Y &&
       ballY + BALL_SIZE <= P2_Y + PADDLE_HEIGHT &&
-      ballX >= p2X && ballX <= p2X + PADDLE_WIDTH) {
-    ballDy = -abs(ballDy);  // Bounce up
+      ballX >= p2X && ballX <= p2X + PADDLE_WIDTH)
+  {
+    ballDy = -abs(ballDy); // Bounce up
   }
 
   // Player 1 missed
-  if (ballY < 0) {
+  if (ballY < 0)
+  {
     p2Score++;
-    if (p2Score == 3) {
+    if (p2Score == 3)
+    {
       gameOver = true;
       winner = 2;
     }
@@ -120,9 +130,11 @@ void move_ball() {
   }
 
   // Player 2 missed
-  if (ballY > SCREEN_HEIGHT) {
+  if (ballY > SCREEN_HEIGHT)
+  {
     p1Score++;
-    if (p1Score == 3) {
+    if (p1Score == 3)
+    {
       gameOver = true;
       winner = 1;
     }
@@ -130,7 +142,8 @@ void move_ball() {
     return;
   }
 }
-void resetGame() {
+void resetGame()
+{
   p1Score = 0;
   p2Score = 0;
   winner = 0;
@@ -142,44 +155,47 @@ void resetGame() {
   tft.fillScreen(TFT_BLACK);
 }
 
-void showWinner() {
-  while(1){
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_YELLOW);
-  tft.setTextSize(3);
-  tft.setCursor(30, SCREEN_HEIGHT / 2 - 20);
-  if (winner == 1) {
-    tft.print("P1 WINS!");
-  } else {
-    tft.print("P2 WINS!");
-  }
+void showWinner()
+{
+  while (1)
+  {
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_YELLOW);
+    tft.setTextSize(3);
+    tft.setCursor(30, SCREEN_HEIGHT / 2 - 20);
+    if (winner == 1)
+    {
+      tft.print("P1 WINS!");
+    }
+    else
+    {
+      tft.print("P2 WINS!");
+    }
 
-  tft.setTextSize(1);
-  tft.setCursor(30, SCREEN_HEIGHT / 2 + 30);
-  tft.print("Press both buttons to restart");
+    tft.setTextSize(1);
+    tft.setCursor(30, SCREEN_HEIGHT / 2 + 30);
+    tft.print("Press both buttons to restart");
 
-  if (digitalRead(BTN_LEFT_1) == LOW && digitalRead(BTN_RIGHT_1) == LOW) {
-    delay(500);
-    return;
-  }
-delay(200);
+    if (digitalRead(BTN_LEFT_1) == LOW && digitalRead(BTN_RIGHT_1) == LOW)
+    {
+      delay(500);
+      return;
+    }
+    delay(200);
   }
 }
 
+void draw_game()
+{
 
-void draw_game(){
- 
   tft.fillRect(prevP1X, P1_Y, PADDLE_WIDTH, PADDLE_HEIGHT, TFT_BLACK);
   tft.fillRect(prevP2X, P2_Y, PADDLE_WIDTH, PADDLE_HEIGHT, TFT_BLACK);
   tft.fillCircle((int)prevBallX, (int)prevBallY, BALL_SIZE, TFT_BLACK);
 
-
   tft.fillRect(p1X, P1_Y, PADDLE_WIDTH, PADDLE_HEIGHT, TFT_BLUE);
   tft.fillRect(p2X, P2_Y, PADDLE_WIDTH, PADDLE_HEIGHT, TFT_GREEN);
 
-
   tft.fillCircle((int)ballX, (int)ballY, BALL_SIZE, TFT_WHITE);
-
 
   tft.fillRect(0, 0, 80, 30, TFT_BLACK);
   tft.fillRect(0, SCREEN_HEIGHT - 30, 80, 30, TFT_BLACK);
@@ -203,10 +219,13 @@ void loop()
   update_paddles();
   move_ball();
   draw_game();
-  if (!gameOver) {
+  if (!gameOver)
+  {
     move_ball();
     draw_game();
-  } else {
+  }
+  else
+  {
     showWinner();
     resetGame();
   }
--- software/games/snake/src/main.cpp
+++ software/games/snake/src/main.cpp
@@ -5,16 +5,15 @@
 #include <OTAServer.h>
 #include "wifi_secrets.h"
 
-#define BTN_UP 16
-#define BTN_DOWN 17
-#define BTN_LEFT 18
-#define BTN_RIGHT 19
+#define BTN_UP 33
+#define BTN_DOWN 34
+#define BTN_LEFT 27
+#define BTN_RIGHT 35
 
 OTAServer otaServer;
 TFT_eSPI tft = TFT_eSPI();
 
-
-//snake globals
+// snake globals
 
 int direction = 4; // 0=up, 1=down, 2=right, 3=left
 bool in_boarder = true;
@@ -28,7 +27,7 @@ int prevSnakeX[SNAKE_MAX];
 int prevSnakeY[SNAKE_MAX];
 int snakeLength = 1;
 int appleX, appleY;
-int rockX,rockY;
+int rockX, rockY;
 bool rock_active = false;
 unsigned long rock_spawn_time = 0;
 int rock_time = 5000;
@@ -50,14 +49,14 @@ void setup()
   pinMode(BTN_RIGHT, INPUT_PULLUP);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  pinMode(21, OUTPUT);
-  digitalWrite(21, HIGH);
-  
-  //tft.init();
-  //tft.fillScreen(TFT_RED);
-  //tft.setCursor(0,0,4);
-  //tft.setTextColor(TFT_BLACK);
-  //tft.println ("Hello World!");
+  pinMode(22, OUTPUT);
+  digitalWrite(22, HIGH);
+
+  // tft.init();
+  // tft.fillScreen(TFT_RED);
+  // tft.setCursor(0,0,4);
+  // tft.setTextColor(TFT_BLACK);
+  // tft.println ("Hello World!");
 
   tft.init();
   tft.fillScreen(TFT_DARKCYAN);
@@ -68,13 +67,10 @@ void setup()
   tft.println("SNAKE GAME");
   delay(2000);
   tft.fillScreen(TFT_DARKCYAN);
-
-
-
-  
 }
 
-void draw_score() {
+void draw_score()
+{
   // tft.fillRect(0, 0, 80, 16, TFT_DARKCYAN);
 
   tft.setCursor(0, 0);
@@ -84,52 +80,75 @@ void draw_score() {
   tft.print(score);
 }
 
-void handle_buttons() {
-  if (digitalRead(BTN_UP) == LOW && direction!=1) direction = 0;
-  if (digitalRead(BTN_DOWN) == LOW && direction!=0) direction = 1;
-  if (digitalRead(BTN_RIGHT) == LOW && direction!=3) direction = 2;
-  if (digitalRead(BTN_LEFT) == LOW && direction!=2) direction = 3;
+void handle_buttons()
+{
+  if (digitalRead(BTN_UP) == LOW && direction != 1)
+    direction = 0;
+  if (digitalRead(BTN_DOWN) == LOW && direction != 0)
+    direction = 1;
+  if (digitalRead(BTN_RIGHT) == LOW && direction != 3)
+    direction = 2;
+  if (digitalRead(BTN_LEFT) == LOW && direction != 2)
+    direction = 3;
 }
 
-void move_snake() {
+void move_snake()
+{
 
-  for (int i = 0; i < snakeLength; i++) {
+  for (int i = 0; i < snakeLength; i++)
+  {
     prevSnakeX[i] = snakeX[i];
     prevSnakeY[i] = snakeY[i];
   }
 
-  for (int i = snakeLength - 1; i > 0; i--) {
+  for (int i = snakeLength - 1; i > 0; i--)
+  {
     snakeX[i] = snakeX[i - 1];
     snakeY[i] = snakeY[i - 1];
   }
 
-  switch (direction) {
-    case 0: snakeY[0] -= 8; break; // up
-    case 1: snakeY[0] += 8; break; // down
-    case 2: snakeX[0] += 8; break; // right
-    case 3: snakeX[0] -= 8; break; // left
+  switch (direction)
+  {
+  case 0:
+    snakeY[0] -= 8;
+    break; // up
+  case 1:
+    snakeY[0] += 8;
+    break; // down
+  case 2:
+    snakeX[0] += 8;
+    break; // right
+  case 3:
+    snakeX[0] -= 8;
+    break; // left
   }
 }
 
-void draw_snake() {
-  for (int i = 0; i < snakeLength; i++) {
+void draw_snake()
+{
+  for (int i = 0; i < snakeLength; i++)
+  {
     tft.fillRect(prevSnakeX[i], prevSnakeY[i], 8, 8, TFT_DARKCYAN);
     tft.fillRect(snakeX[i], snakeY[i], 8, 8, TFT_GREEN);
-    
   }
 }
 
-void draw_apple() {
-  if (!is_apple) {
+void draw_apple()
+{
+  if (!is_apple)
+  {
     bool valid = false;
 
-    while (!valid) {
+    while (!valid)
+    {
       appleX = random(2, 29) * 8;
       appleY = random(2, 28) * 8;
       valid = true;
 
-      for (int i = 0; i < snakeLength; i++) {
-        if (snakeX[i] == appleX && snakeY[i] == appleY) {
+      for (int i = 0; i < snakeLength; i++)
+      {
+        if (snakeX[i] == appleX && snakeY[i] == appleY)
+        {
           valid = false;
           break;
         }
@@ -142,42 +161,50 @@ void draw_apple() {
   tft.fillRect(appleX, appleY, 8, 8, TFT_RED);
 }
 
-void check_apple() {
-  if (abs(snakeX[0] - appleX) < 8 && abs(snakeY[0] - appleY) < 8) {
-    if (snakeLength < SNAKE_MAX) snakeLength++;
+void check_apple()
+{
+  if (abs(snakeX[0] - appleX) < 8 && abs(snakeY[0] - appleY) < 8)
+  {
+    if (snakeLength < SNAKE_MAX)
+      snakeLength++;
     is_apple = false;
     tft.fillRect(appleX, appleY, 8, 8, TFT_DARKCYAN);
     score++;
-    tft.fillRect(50, 0,60 , 16, TFT_DARKCYAN);
+    tft.fillRect(50, 0, 60, 16, TFT_DARKCYAN);
     draw_score();
   }
 
-  if (score % 2 == 0 && score != last_score_for_speed && game_speed > 50) {
+  if (score % 2 == 0 && score != last_score_for_speed && game_speed > 50)
+  {
     game_speed -= 10;
     last_score_for_speed = score;
   }
-
 }
 
-void check_collision() {
-  if (snakeX[0] < 0 || snakeX[0] > 232 || snakeY[0] < 0 || snakeY[0] > 312) {
+void check_collision()
+{
+  if (snakeX[0] < 0 || snakeX[0] > 232 || snakeY[0] < 0 || snakeY[0] > 312)
+  {
     in_boarder = false;
   }
 
-  for (int i = 1; i < snakeLength; i++) {
-    if (snakeX[0] == snakeX[i] && snakeY[0] == snakeY[i]) {
+  for (int i = 1; i < snakeLength; i++)
+  {
+    if (snakeX[0] == snakeX[i] && snakeY[0] == snakeY[i])
+    {
       colision = true;
     }
   }
 }
 
-void game_over() {
+void game_over()
+{
   tft.fillScreen(TFT_BLACK);
   tft.setCursor(40, 80);
   tft.setTextSize(2);
   tft.setTextColor(TFT_RED);
   tft.println("GAME OVER");
-  
+
   tft.setCursor(40, 110);
   tft.setTextColor(TFT_WHITE);
   tft.print("Score: ");
@@ -186,44 +213,52 @@ void game_over() {
   tft.setCursor(20, 140);
   tft.println("Press any key");
 
-
   game_over_flag = true;
 }
-void draw_rock() {
+void draw_rock()
+{
   bool valid = false;
-  while(!valid){
+  while (!valid)
+  {
     rockX = random(2, 29) * 8;
     rockY = random(2, 28) * 8;
     valid = true;
-    for(int i=0;i<snakeLength;i++){
-      if((snakeX[i]==rockX && snakeY[i]==rockY)||(appleX==rockX && appleY==rockY)){
+    for (int i = 0; i < snakeLength; i++)
+    {
+      if ((snakeX[i] == rockX && snakeY[i] == rockY) || (appleX == rockX && appleY == rockY))
+      {
         valid = false;
         break;
       }
     }
   }
-  tft.fillRect(rockX,rockY,12,12,TFT_BLACK);
+  tft.fillRect(rockX, rockY, 12, 12, TFT_BLACK);
   rock_active = true;
   rock_spawn_time = millis();
 }
 
-void handle_rock(){
+void handle_rock()
+{
   unsigned long current_time = millis();
-  if(!rock_active && current_time - last_rock_spawn >= rock_interval){
+  if (!rock_active && current_time - last_rock_spawn >= rock_interval)
+  {
     draw_rock();
     last_rock_spawn = current_time;
   }
 
-  if(rock_active && current_time - last_rock_spawn >= rock_time){
-    tft.fillRect(rockX,rockY,12,12,TFT_DARKCYAN);
+  if (rock_active && current_time - last_rock_spawn >= rock_time)
+  {
+    tft.fillRect(rockX, rockY, 12, 12, TFT_DARKCYAN);
     rock_active = false;
   }
 
-  if (abs(snakeX[0] - rockX) < 12 && abs(snakeY[0] - rockY) < 12) {
+  if (abs(snakeX[0] - rockX) < 12 && abs(snakeY[0] - rockY) < 12)
+  {
     game_over();
   }
 }
-void reset_game() {
+void reset_game()
+{
   direction = 4;
   in_boarder = true;
   colision = false;
@@ -241,16 +276,19 @@ void reset_game() {
 void loop()
 {
 
-  if (game_over_flag) {
-    if (digitalRead(BTN_UP) == LOW || digitalRead(BTN_DOWN) == LOW || 
-        digitalRead(BTN_LEFT) == LOW || digitalRead(BTN_RIGHT) == LOW) {
+  if (game_over_flag)
+  {
+    if (digitalRead(BTN_UP) == LOW || digitalRead(BTN_DOWN) == LOW ||
+        digitalRead(BTN_LEFT) == LOW || digitalRead(BTN_RIGHT) == LOW)
+    {
       reset_game();
     }
     delay(100);
     return;
   }
 
-  if (!in_boarder || colision) {
+  if (!in_boarder || colision)
+  {
     game_over();
     return;
   }
@@ -265,14 +303,13 @@ void loop()
   handle_rock();
   check_collision();
 
-
   delay(game_speed);
 
-  //if (digitalRead(BTN_UP) == LOW) Serial.println("Button 1 pressed");
-  //if (digitalRead(BTN_DOWN) == LOW) Serial.println("Button 2 pressed");
-  //if (digitalRead(BTN_LEFT) == LOW) Serial.println("Button 3 pressed");
-  //if (digitalRead(BTN_RIGHT) == LOW) Serial.println("Button 4 pressed");
-  //delay(100);
+  // if (digitalRead(BTN_UP) == LOW) Serial.println("Button 1 pressed");
+  // if (digitalRead(BTN_DOWN) == LOW) Serial.println("Button 2 pressed");
+  // if (digitalRead(BTN_LEFT) == LOW) Serial.println("Button 3 pressed");
+  // if (digitalRead(BTN_RIGHT) == LOW) Serial.println("Button 4 pressed");
+  // delay(100);
   otaServer.connectionLoop();
   otaServer.handleHttpClient();
   otaServer.handleDiscoveries();
